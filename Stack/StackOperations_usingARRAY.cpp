003c397730a7c85151da84c3d3694dd0eea38c38
Anuj Das <53907044+anuj-das-10@users.noreply.github.com>
2021-11-01 01:51:18 +0530

    Updated Code!

--- Stack/StackOperations_usingARRAY.cpp
+++ Stack/StackOperations_usingARRAY.cpp
@@ -2,85 +2,84 @@
 #include <iostream>
 #define MAX 5
 using namespace std;
+int stack[MAX] , Top= -1;
 
-class Stack 
-{
-	int top;
+void Push(int x);
+void Pop();
 
-	public:
-		int a[MAX]; 
-		Stack() 
-		{ 
-			top = -1; 
-		}
-	
-	bool PUSH(int x);
-	int POP();
-	int peek();
-	bool isEmpty();
-};
-
-bool Stack::PUSH(int x)
+void Display_Stack()
 {
-	if (top >= (MAX - 1)) {
-		cout << "STACK OVERFLOW..!!";
-		return false;
+	if(Top>=0){
+		cout<<"Elements present in the Stack: ";
+		for(int i=Top; i>=0; i--){
+			cout<<stack[i]<<"  ";
+		}	
+		cout<<endl<<endl;
 	}
-	else {
-		a[++top] = x;
-		cout << x << " PUSHED into the Stack!!\n";
-		return true;
+	else{
+		cout<<"Stack is Empty!!"<<endl<<endl;
 	}
-}
-
-int Stack::POP()
-{
-	if (top < 0) {
-		cout << "STACK UNDERFLOW..!!";
-		return 0;
-	}
-	else {
-		int x = a[top--];
-		return x;
-	}
-}
-int Stack::peek()
-{
-	if (top < 0) {
-		cout << "Stack is Empty!";
-		return 0;
-	}
-	else {
-		int x = a[top];
-		return x;
-	}
-}
-
-bool Stack::isEmpty()
-{
-	return (top < 0);
 }
 
 
 int main()
 {
-	class Stack stack;
-	stack.PUSH(10);
-	stack.PUSH(20);
-	stack.PUSH(30);
-	stack.PUSH(56);
-	cout << endl << stack.POP() << " POPPED from the Stack!!\n";
-
-	//Print all elements in stack :
-	cout<<"Elements Present in the Stack : ";
-	while(!stack.isEmpty())
+	int choice , x;
+	do{
+	cout<<"<------- STACK OPERATIONS ------->"<<endl;
+	cout<<"1. PUSH Elements\n2. POP Elements\n3. Display Elements in Stack\n4. Exit"<<endl;
+	cout<<"<-------------------------------->"<<endl;
+	cout<<"\nEnter your choice: ";
+	cin>>choice;
+	switch(choice)
 	{
-		// Print top element in stack
-		cout<<stack.peek()<<" ";
-		// Remove top element in stack
-		stack.POP();
+		case 1:		cout<<"Enter Element to PUSH: ";
+					cin>>x;
+					system("cls");
+					Push(x);
+					break;
+		
+		case 2:     system("cls");
+					Pop();
+					break;
+					
+	    case 3:		system("cls");
+					cout<<endl;
+					Display_Stack();
+					break;
+			
+		case 4:		system("cls");
+					cout<<"\nExiting..."<<endl;
+					exit(0);
+		
+		default:	system("cls");
+					cout<<"\nInvalid Choice!!"<<endl<<endl;
 	}
-
+	}while(choice!=4);
 	return 0;
 }
 
+void Push(int x)
+{
+	if(Top>= MAX-1){
+		cout<<"\nStack Overflow..!!"<<endl<<endl;
+	}
+	else{
+		stack[++Top]=x;
+		cout<<endl<<stack[Top] <<" PUSHED to the Stack!!"<<endl;
+		Display_Stack();
+	}
+}
+
+void Pop()
+{
+	if(Top<0){
+		
+		cout<<"\nStack Underflow..!!"<<endl<<endl;
+	}
+	else{
+		cout<<endl<<stack[Top]<<" POPPED from the Stack!!"<<endl;
+		Top--;
+		Display_Stack();
+	}
+}
