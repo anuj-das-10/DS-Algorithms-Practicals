305648a59b1a28858311057bbd50664b8ed983f9
Anuj Das <53907044+anuj-das-10@users.noreply.github.com>
2021-11-06 20:50:19 +0530

    Code Updated!

--- Array/Insertion&Deletion_Element_Array.cpp
+++ Array/Insertion&Deletion_Element_Array.cpp
@@ -1,21 +1,31 @@
 //Program to implement insertion and deletion of an element in an Array.
 #include<iostream>
+#define MAXSIZE 100
 using namespace std;
 
+
 void Display_Array(int arr[], int size);
-int Insert_Element(int arr[], int size, int index, int element);
-int Delete_Element(int arr[], int size, int index);
+void Insert_Element(int arr[], int size, int index, int element);
+void Delete_Element(int arr[], int size, int index);
 
 int main()
 {
-	int arr[]={23,3,4,56,67,21,34,66,33};
-	int size = sizeof(arr)/sizeof(int);
+	int arr[MAXSIZE],size;
 	int index, element, choice;
-	cout<<"Given Array: ";
+	cout<<"Enter the Number of Element you want to Enter: ";
+	cin>>size;
+	cout<<"Enter "<<size<<" Elements: "<<endl;
+	for(int i=0; i<size; i++){
+	   cin>>arr[i];
+	}
+	system("cls");
+	cout<<"GIVEN ARRAY: ";
 	Display_Array(arr,size);
-	menu:
+	do{
+	cout<<"<------ ARRAY OPERATIONS ------>"<<endl;
 	cout<<"1. Insert Element\n2. Delete Element\n3. Exit"<<endl;
-	cout<<"Enter your Choice: ";
+	cout<<"<------------------------------>"<<endl;
+	cout<<"\nEnter your Choice: ";
 	cin>>choice;
 	
 	switch(choice)
@@ -24,30 +34,36 @@ int main()
 					cin>>element;
 					cout<<"Enter the Position of the Element to be Inserted: ";
    					cin>>index;
+   					size++;
    					Insert_Element(arr,size,index,element);
+   					system("cls");
+   					cout<<"\nArray After Insertion of "<<element<<" at Position: "<<index<<endl;
    					Display_Array(arr,size);
 					break;
 	
 		case 2: 	cout<<"Enter the Position of the Element to be Deleted: ";
    					cin>>index;
    					Delete_Element(arr,size,index);
+   					size--;
+   					system("cls");
+   					cout<<"\nArray After Deletion of Element at Position: "<<index<<endl;
    					Display_Array(arr,size);
 					break;
    		
    		case 3:         cout<<"Exiting.....";
-		   		exit(0);
+		   			exit(0);
 		   			
 	       default: 	system("cls");
 					cout<<"Invalid Choice!!\nChoose Again!!"<<endl; 
 					cout<<"\nGiven Array: ";
-	    			Display_Array(arr,size);
-				goto menu;
+	    			        Display_Array(arr,size);
+				 	
    }
-   
+}while(choice!=3);
    	return 0;		
 }
 
-int Insert_Element(int arr[], int size, int index, int element)
+void Insert_Element(int arr[], int size, int index, int element)
 {
 	for(int i=size-1; i>=index-1; i--)
 	{
@@ -57,7 +73,7 @@ int Insert_Element(int arr[], int size, int index, int element)
 }
 
 
-int Delete_Element(int arr[], int size, int index)
+void Delete_Element(int arr[], int size, int index)
 {
 	for(int i=index-1; i<size-1; i++ )
 	{
@@ -74,21 +90,4 @@ void Display_Array(int arr[], int size)
 	}
 	cout<<endl<<endl;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
